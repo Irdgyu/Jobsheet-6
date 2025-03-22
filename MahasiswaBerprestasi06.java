38941dc3b43da1a539403d6b3ebff22148c9e186
Irdgyu <gilangbayuirwana@gmail.com>
2025-03-22 22:32:24 +0700

    Jobsheet 6

--- MahasiswaBerprestasi06.java
+++ MahasiswaBerprestasi06.java
@@ -1,36 +1,43 @@
 public class MahasiswaBerprestasi06 {
-    Mahasiswa06[] listMhs;
+    Mahasiswa06 [] listMhs = new Mahasiswa06[5];
     int idx;
 
-    MahasiswaBerprestasi06(int kapasitas) {
-        listMhs = new Mahasiswa06[kapasitas];
-        idx = 0;
-    }
     void tambah(Mahasiswa06 m) {
-        if (idx < listMhs.length) {
-            listMhs[idx] = m;
+        if (idx<listMhs.length) {
+            listMhs[idx]=m;
             idx++;
-        } else {
-            System.out.println("Data sudah penuh!");
+        }else {
+            System.out.println("Data sudah penuh");
         }
     }
     void tampil() {
-        for (Mahasiswa06 m : listMhs) {
-            if (m != null) { 
-                m.tampilInformasi();
-                System.out.println("---------------------------");
-            }
+        for (Mahasiswa06 m:listMhs) {
+            m.tampilInformasi();
+            System.out.println("--------");
         }
     }
     void bubbleSort() {
-        for (int i = 0; i < idx - 1; i++) {
-            for (int j = 0; j < idx - 1 - i; j++) {
-                if (listMhs[j].ipk < listMhs[j + 1].ipk) { 
+        for (int i=0; i<listMhs.length-1; i++) {
+            for (int j=1; j<listMhs.length-i; j++) {
+                if (listMhs[j].ipk>listMhs[j-1].ipk) {
                     Mahasiswa06 tmp = listMhs[j];
-                    listMhs[j] = listMhs[j + 1];
-                    listMhs[j + 1] = tmp;
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
                 }
             }
+        }
+    }
+    void selectionSort() {
+        for (int i=0; i<listMhs.length-1; i++) {
+            int idxMin = i;
+            for (int j=i+1; j<listMhs.length; j++) {
+                if (listMhs[j].ipk<listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            Mahasiswa06 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
         }
     }
 }
--- MahasiswaDemo06.java
+++ MahasiswaDemo06.java
@@ -1,33 +1,27 @@
-import java.util.Scanner;
 public class MahasiswaDemo06 {
     public static void main(String[] args) {
-        Scanner input6 = new Scanner(System.in);
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = input6.nextInt();
-        input6.nextLine(); 
+        MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
+        Mahasiswa06 m1 = new Mahasiswa06("123","Zidan","2A",3.2);
+        Mahasiswa06 m2 = new Mahasiswa06("124","Ayu","2A",3.5);
+        Mahasiswa06 m3 = new Mahasiswa06("125","Sofi","2A",3.1);
+        Mahasiswa06 m4 = new Mahasiswa06("126","Sita","2A",3.9);
+        Mahasiswa06 m5 = new Mahasiswa06("127","Miki","2A",3.7);
 
-        MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06(jumlah);
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
 
-        for (int i = 0; i < jumlah; i++) {
-            System.out.println("\nMahasiswa ke-" + (i + 1));
-            System.out.print("Masukkan NIM   : ");
-            String nim = input6.nextLine();
-            System.out.print("Masukkan Nama  : ");
-            String nama = input6.nextLine();
-            System.out.print("Masukkan Kelas : ");
-            String kelas = input6.nextLine();
-            System.out.print("Masukkan IPK   : ");
-            double ipk = input6.nextDouble();
-            input6.nextLine();
-
-            Mahasiswa06 mhs = new Mahasiswa06(nim, nama, kelas, ipk);
-            list.tambah(mhs);
-        }
-
-        System.out.println("\nData mahasiswa sebelum disorting:");
+        System.out.println("Data mahasiswa sebelum disorting: ");
         list.tampil();
-        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC) :");
+
+        System.out.println("Data mahasiswa setelah sorting  berdasarkan IPK (DESC) : ");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data mahasiswa yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
         list.tampil();
     }
 }
